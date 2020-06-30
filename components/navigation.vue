ee7adc717db05dd56c6dddadc94a51e722cfd07f
Lindakat <lindakt16@gmail.com>
2020-06-30 16:22:22 -0500

    Removed boilerplate files, added blank files

---
+++ components/navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Navigation bar!!
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -22,13 +22,15 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Acme&family=Bree+Serif&display=swap'},
     ]
   },
   /*
   ** Global CSS
   */
   css: [
+    '@/assets/styles.css'
   ],
   /*
   ** Plugins to load before mounting the App
