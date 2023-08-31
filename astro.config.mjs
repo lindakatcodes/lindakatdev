d3251309d23603c8a410efebdac5cf3f8780c682
LindaKat <lindakt16@gmail.com>
2023-08-31 16:59:39 -0500

    updated to astro v3; updated theme toggle logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-    viewTransitions: true,
-  },
+
 });
