cddde826f5566d54e06ad0712b7e8fb53ae4f57f
LindaKat <lindakt16@gmail.com>
2023-11-08 01:53:07 -0600

    added read time and tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./src/util/remarkReadingTime.mjs";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lindakat.com',
+  site: "https://lindakat.com",
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
       theme: "rose-pine-moon",
       wrap: true,
     },
+    remarkPlugins: [remarkReadingTime],
   },
 });
