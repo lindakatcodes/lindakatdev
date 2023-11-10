5ad5ea80057aed708d54bfca5aa2c79e5a30507d
Linda Thompson <lindakt16@gmail.com>
2023-11-10 01:59:18 -0600

    changed default og image

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_METADATA = {
   title: "LindaKat Codes",
   description: "The online home of all my code, thoughts, and more.",
-  imagePath: "/og-image.png",
+  imagePath: "/default-og-image.png",
   url: "https://lindakat.com",
 };
