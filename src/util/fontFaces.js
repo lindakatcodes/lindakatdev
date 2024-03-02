0839c3bfe3e58b2bb1ff678961711ab47f035900
LindaKat <lindakt16@gmail.com>
2024-03-02 02:53:22 -0600

    linted im so sorry

--- src/util/fontFaces.js
+++ src/util/fontFaces.js
@@ -1,6 +1,6 @@
 const birthstoneFontFace = new FontFace(
   "Birthstone",
-  "url(https://fonts.gstatic.com/s/birthstone/v14/8AtsGs2xO4yLRhy87sv_LL7zjA.woff2)"
+  "url(https://fonts.gstatic.com/s/birthstone/v14/8AtsGs2xO4yLRhy87sv_LL7zjA.woff2)",
 );
 
 document.fonts.add(birthstoneFontFace);
