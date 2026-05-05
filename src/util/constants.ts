fb48845582caa6def05258b2e303ed9ec0f1e951
LindaKat <lindakt16@gmail.com>
2026-05-04 19:29:03 -0500

    setting up baseline styles and layout

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,9 +1,8 @@
 export const DEFAULT_METADATA = {
   title: "LindaKat Codes",
-  description: "The online home of all my code, thoughts, and more.",
+  description: "Where Linda shows off her projects and musings.",
   imagePath: "/default-og-image.png",
   url: "https://lindakat.com",
 };
 
 export const EMAIL = "lindakt16@gmail.com";
-// export const EMAIL = "hello@lindakat.com";
